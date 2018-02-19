2f9f65fe102577f425ef596b2e25e713a8310eb9
laurent <laurentm@magelo.com>
2018-02-19 17:13:53 +0100

    fix(shared): refactored css class name

--- capa_planning_front/src/user/user.routing.ts
+++ capa_planning_front/src/user/user.routing.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { ListUserComponent } from './list-user/list-user.component';
 import { ViewUserComponent } from './view-user/view-user.component';
+import { ManagerUserComponent } from './manager-user/manager-user.component';
 
 const routes: Routes = [
   { path: 'createUser', component: CreateUserComponent },
   { path: 'viewUser/:id', component: ViewUserComponent },
   { path: 'users', component: ListUserComponent },
+  { path: 'manage', component: ManagerUserComponent },
 ];
 
 export const UserRoutes = RouterModule.forChild(routes);
