db2d15c4f2216398c26fa210e133601e26cae64d
laurent <laurentm@magelo.com>
2018-02-28 17:09:43 +0100

    fix(user): Removed the on_delete on the user_working_day schema

--- capa_planning_back/priv/repo/migrations/20180227093049_create_user_working_days.exs
+++ capa_planning_back/priv/repo/migrations/20180227093049_create_user_working_days.exs
@@ -3,7 +3,7 @@ defmodule CapaPlanning.Repo.Migrations.CreateUserWorkingDays do
 
   def change do
     create table(:user_working_days) do
-      add(:user_id, references("users", on_delete: :delete_all))
+      add(:user_id, references("users"))
       add(:day_id, references("days"))
       add(:worked, :boolean, default: false, null: false)
 
